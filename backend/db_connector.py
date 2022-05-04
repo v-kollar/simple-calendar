234e38b85f21e8df4c164640eb389c829dd68fdc
Vojtěch Kollár <xkollar3@mendelu.cz>
2022-05-04 13:51:38 +0200

    Light changes; added function returning data in JSON format on localhost

--- backend/db_connector.py
+++ backend/db_connector.py
@@ -1,29 +1,79 @@
 #!/usr/bin/env python3
-import mysql.connector
-from typing import Callable
 
-db = mysql.connector.connect(
+from flask import Flask
+from flask_restful import Api
+import mysql.connector as mysql
+from typing import Any  # Dict, List
+import json
+
+Action = Any
+JSON = str  # List[Dict[str, Any]]
+
+app = Flask(__name__)
+api = Api(app)
+# SELECT * FROM calendar.user
+
+db = mysql.connect(
     database="calendar",
     host="::1",
-    user="admin",
-    passwd="localhostapp."
+    user="root",
+    passwd="Rootroot.000"
 )
 
-def db_exec(command: str) -> Callable:
-    db.cursor().execute(f"{command}")
+cursor = db.cursor(dictionary=True)
 
-def create_table(name: str) -> Callable:
+
+def db_command(command: str) -> Action:
+    """
+    Executes commands via SQL queries.
+    :param command:
+    :return:
+    """
+    cursor.execute(f"{command}")
+
+
+def create_table(name: str) -> Action:
+    """
+    Creates table in SQL database.
+    :param name:
+    :return:
+    """
     try:
-        db.cursor().execute(f"CREATE TABLE {name}")
+        cursor.execute(f"CREATE TABLE {name}")
         print(f"Table {name} has been created.")
-    except mysql.connector.DatabaseError:
-        print("Table must have at least one column.")
+    except mysql.DatabaseError as error:
+        print(f"Something went wrong: {error}")
 
-def delete_table(name: str) -> Callable:
+
+def delete_table(name: str) -> Action:
+    """
+    Deletes table in SQL database.
+    :param name:
+    :return:
+    """
     try:
-        db.cursor().execute(f"DROP TABLE {name}")
+        cursor.execute(f"DROP TABLE {name}")
         print(f"Table {name} has been deleted.")
-    except mysql.connector.Error:
-        print(f"Can't create table '{name}', because it does not exist.")
+    except mysql.DatabaseError as error:
+        print(f"Something went wrong: {error}")
 
-# db_exec("CREATE TABLE user (name VARCHAR(255), surname VARCHAR(255))")
+
+# db_exec("CREATE TABLE calendar (id INTEGER, name VARCHAR(255))")
+
+db_command('SELECT * FROM calendar.calendar')
+
+
+@app.route('/')
+def get_data() -> JSON:
+    """
+    Converts SQL data into JSON.
+    :return: JSON
+    """
+    try:
+        json_data = json.dumps(cursor.fetchall())
+        return json_data
+    except mysql.DatabaseError as error:
+        print(f"Something went wrong: {error}")
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
