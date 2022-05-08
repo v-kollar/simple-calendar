7e3b6e00e9b639c69a943d2db32f8176f74584b2
Vojtěch Kollár <xkollar>
2022-05-08 22:52:42 +0200

    Edited in compliance with PEP8 and Mypy

--- backend/db_connector.py
+++ backend/db_connector.py
@@ -1,95 +1,135 @@
-from time import strftime
-from tracemalloc import start
+#!/usr/bin/env python3
+from typing import List, Dict, Callable, Any
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 import datetime
-from datetime import datetime, timedelta
+from datetime import timedelta
+
+JSON = List[Dict[str: Any]]
 
 app = Flask(__name__)
-#Origin cors
+
+# Origin cors
 CORS(app)
 
-# Databse configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456789@localhost/calendar'
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'mysql://root:123456789@localhost/calendar'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-ma=Marshmallow(app)
+ma = Marshmallow(app)
+
 
 class Tasks(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    startDate = db.Column(db.DateTime)
-    endDate = db.Column(db.DateTime)
-    rRule = db.Column(db.String(255))
-    allDay = db.Column(db.Boolean)
+    id: int = db.Column(db.Integer, primary_key=True)
+    title: str = db.Column(db.String(255))
+    startDate: str = db.Column(db.DateTime)
+    endDate: str = db.Column(db.DateTime)
+    rRule: str = db.Column(db.String(255))
+    allDay: bool = db.Column(db.Boolean)
 
     def __init__(self, title, startDate, endDate, allDay):
-        self.title=title
-        self.startDate=startDate
-        self.endDate=endDate
-        self.allDay=allDay
+        self.title = title
+        self.startDate = startDate
+        self.endDate = endDate
+        self.allDay = allDay
 
 
 class TasksSchema(ma.Schema):
     class Meta:
-        fields = ('id','title','startDate','endDate','rRule','allDay')
+        fields = ('id', 'title', 'startDate',
+                  'endDate', 'rRule', 'allDay')
+
 
 task_schema = TasksSchema()
 tasks_schema = TasksSchema(many=True)
 
+
 @app.route('/get', methods=['GET'])
-def get_tasks():
+def get_tasks() -> JSON:
+    """
+    Gets all tasks from database
+    :return: tasks
+    """
     all_tasks = Tasks.query.all()
     results = tasks_schema.dump(all_tasks)
     return jsonify(results)
 
+
 @app.route('/get/<id>', methods=['GET'])
-def get_task(id):
-    task = Tasks.query.get(id)
+def get_task(taskID) -> JSON:
+    """
+    Gets single task out of database
+    :param taskID: ID of task in database
+    :return:
+    """
+    task = Tasks.query.get(taskID)
     return task_schema.jsonify(task)
 
-@app.route('/update/<id>',methods = ['PUT'])
-def update_task(id):
-    task = Tasks.query.get(id)
-    if('title' in request.json):
+
+@app.route('/update/<id>', methods=['PUT'])
+def update_task(taskID) -> Callable:
+    """
+    Updates task in database
+    :param taskID: ID of task in database
+    :return:
+    """
+    task = Tasks.query.get(taskID)
+    if 'title' in request.json:
         title = request.json['title']
         task.title = title
-    if('allDay' in request.json):
+    if 'allDay' in request.json:
         allDay = request.json['allDay']
         task.allDay = allDay
-    if('startDate' in request.json):
-        startDate = datetime.strptime(request.json['startDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    if 'startDate' in request.json:
+        startDate = datetime.strptime(request.json['startDate'],
+                                      '%Y-%m-%dT%H:%M:%S.%fZ')
         startDate = startDate + timedelta(hours=2)
         task.startDate = startDate
-    if('endDate' in request.json):
-        endDate = datetime.strptime(request.json['endDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    if 'endDate' in request.json:
+        endDate = datetime.strptime(request.json['endDate'],
+                                    '%Y-%m-%dT%H:%M:%S.%fZ')
         endDate = endDate + timedelta(hours=2)
         task.endDate = endDate
     db.session.commit()
     return get_tasks()
 
-@app.route('/delete/<id>',methods=['DELETE'])
-def delete_task(id):
-    task = Tasks.query.get(id)
+
+@app.route('/delete/<id>', methods=['DELETE'])
+def delete_task(taskID) -> Callable:
+    """
+    Deletes task from database.
+    :param taskID: ID of task in database
+    :return:
+    """
+    task = Tasks.query.get(taskID)
     db.session.delete(task)
     db.session.commit()
     return get_tasks()
 
-@app.route('/add',methods=['POST'])
-def add_task():
+
+@app.route('/add', methods=['POST'])
+def add_task() -> Callable:
+    """
+    Adds task to database
+    :return:
+    """
     title = request.json['title']
     allDay = request.json['allDay']
 
-    #'2022-05-07T00:00:24.404Z'
-    startDate = datetime.strptime(request.json['startDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
-    endDate = datetime.strptime(request.json['endDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    # '2022-05-07T00:00:24.404Z'
+    startDate = datetime.strptime(
+        request.json['startDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    endDate = datetime.strptime(
+        request.json['endDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
 
+    # Added additional two hours due to time zones.
     startDate = startDate + timedelta(hours=2)
     endDate = endDate + timedelta(hours=2)
-    #Timezone
+
     tasks = Tasks(title, startDate, endDate, allDay)
 
     db.session.add(tasks)
@@ -97,5 +137,5 @@ def add_task():
     return get_tasks()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
